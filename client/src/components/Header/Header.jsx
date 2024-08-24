8c4721a7f8c7929fedd7f5a0933b0bb9121d2439
iishu <ishikasrivastava27594@gmail.com>
2024-08-24 23:54:02 +0530

    commit flipkart logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+// import React from 'react';
+import {AppBar, Toolbar, Box, styled} from '@mui/material'
+const StyledHeader=styled(AppBar)`
+    background:#2874f0;
 
+`;
+const Component=styled(Box)`
+margin-left:12%;
+`
 const Header = () => {
+    const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+
   return (
-    <div>Hello from Header</div>
+    <StyledHeader>
+        <Toolbar>
+            <Component>
+                <img src={logoURL} alt="logo" style={{ width:75}}/>
+                <Box>
+                    <p>Explore</p>
+                </Box>
+            </Component>
+        </Toolbar>
+    </StyledHeader>
   )
 }
 
